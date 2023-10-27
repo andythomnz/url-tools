35d89fe7374009ee153f8b6308052f531e38efe6
Andrew Thompson <andy@thomnz.com>
2023-10-27 18:26:29 +0100

    Added 404 Not Found status when WebLink key does not exist

--- app/controllers/web_links_controller.rb
+++ app/controllers/web_links_controller.rb
@@ -15,6 +15,8 @@ class WebLinksController < ApplicationController
   end
 
   def set_web_link
-    @web_link = WebLink.find_by_key(redirect_params)
+    @web_link = WebLink.find_by_key!(redirect_params)
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
   end
 end
--- spec/requests/web_links_spec.rb
+++ spec/requests/web_links_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 RSpec.describe "WebLinks", type: :request do
   describe "#redirect" do
     let(:web_link) { create(:google_web_link) }
+    let(:key) { web_link.key }
 
     before do
-      get "/r/#{web_link.key}"
+      get "/r/#{key}"
     end
 
-    it "HTTP redirects the key to the destination" do
-      expect(response).to redirect_to web_link.destination
+    context "to a valid key" do
+      it "HTTP redirects the key to the destination" do
+        expect(response).to redirect_to web_link.destination
+      end
+    end
+
+    context "to a non-existent key" do
+      let(:key) { "FAKE" }
+
+      it { expect(response).to be_not_found }
     end
   end
 end
