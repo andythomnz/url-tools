1c75e6f5bd78450e9f6185c637190696827005b0
Andrew Thompson <andy@thomnz.com>
2023-10-27 19:19:57 +0100

    Added extremely basic form to create new WebLink

--- app/controllers/web_links_controller.rb
+++ app/controllers/web_links_controller.rb
@@ -8,10 +8,25 @@ class WebLinksController < ApplicationController
     redirect_to @web_link.destination, allow_other_host: true
   end
 
+  def create
+    @web_link = WebLink.build(create_params)
+
+    @web_link.save!
+    redirect_to root_url
+  end
+
+  def new
+    @web_link = WebLink.new
+  end
+
   private
 
   def redirect_params
     params.require(:key)
+  end
+
+  def create_params
+    params.require(:web_link).permit(:key, :destination)
   end
 
   def set_web_link
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root "web_links#index"
+  root "web_links#new"
 
-  get "/web_links", to: "web_links#index"
+  get "/web_links", to: "web_links#new"
   get "/r/:key", to: "web_links#redirect"
+
+  resources :web_links, only: [:new, :create]
 end
