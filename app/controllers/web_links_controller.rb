f92431c2232a0cd4d4a95b3fe7bb87a330d750ff
Andrew Thompson <andy@thomnz.com>
2023-10-29 17:44:14 +0000

    Create very basic frontend (#2)
    * Added 404 Not Found status when WebLink key does not exist
    
    * Added extremely basic form to create new WebLink
    
    * Added some more tests
    
    * Crudely space out form inputs

--- app/controllers/web_links_controller.rb
+++ app/controllers/web_links_controller.rb
@@ -8,13 +8,33 @@ class WebLinksController < ApplicationController
     redirect_to @web_link.destination, allow_other_host: true
   end
 
+  def create
+    @web_link = WebLink.build(create_params)
+
+    if @web_link.save
+      redirect_to root_url
+    else
+      render json: @web_link.errors, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @web_link = WebLink.new
+  end
+
   private
 
   def redirect_params
     params.require(:key)
   end
 
+  def create_params
+    params.require(:web_link).permit(:key, :destination)
+  end
+
   def set_web_link
-    @web_link = WebLink.find_by_key(redirect_params)
+    @web_link = WebLink.find_by_key!(redirect_params)
+  rescue ActiveRecord::RecordNotFound
+    head :not_found
   end
 end
--- spec/requests/web_links_spec.rb
+++ spec/requests/web_links_spec.rb
@@ -3,13 +3,40 @@ require 'rails_helper'
 RSpec.describe "WebLinks", type: :request do
   describe "#redirect" do
     let(:web_link) { create(:google_web_link) }
+    let(:key) { web_link.key }
 
-    before do
-      get "/r/#{web_link.key}"
+    before { get "/r/#{key}" }
+
+    context "to a valid key" do
+      it "HTTP redirects the key to the destination" do
+        expect(response).to redirect_to web_link.destination
+      end
     end
 
-    it "HTTP redirects the key to the destination" do
-      expect(response).to redirect_to web_link.destination
+    context "to a non-existent key" do
+      let(:key) { "FAKE" }
+
+      it { expect(response).to be_not_found }
+    end
+  end
+
+  describe "#create" do
+    subject { -> { post "/web_links", params: { "web_link" => params } }}
+    before { subject.call }
+
+    context "with valid parameters" do
+      let(:params) { { "key" => "ABC123", "destination" => "https://example.com" } }
+
+      it { expect(WebLink.find_by_key(params["key"])).to be_present }
+      it { expect(WebLink.last.attributes).to include params }
+      it { expect(response).to redirect_to "/" }
+    end
+
+    context "with invalid parameters" do
+      let(:params) { { key: "AAAAAAAAAAAAAAAA", destination: "example" } }
+
+      it { expect{subject.call}.to_not change{WebLink.count} }
+      it { expect(response).to have_http_status :unprocessable_entity }
     end
   end
 end
