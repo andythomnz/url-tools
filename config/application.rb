c8e4afb3ae8166367e07489db8931053d967df16
Andrew Thompson <andy@thomnz.com>
2023-10-29 18:12:18 +0000

    Setup basic attack protection with Rack Attack (#3)

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module UrlTools
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.middleware.use Rack::Attack
   end
 end
